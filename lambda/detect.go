5490becd06b082ec481f56e8dec2edfec79a2d36
Wilson da Rocha Franca <wilsonrf@gmail.com>
2024-11-22 11:14:28 +0100

    implement detect

--- lambda/detect.go
+++ lambda/detect.go
@@ -4,6 +4,21 @@ import "github.com/paketo-buildpacks/packit/v2"
 
 type Detect struct{}
 
+const (
+	PlanEntryAwsLambda             = "aws-lambda"
+	PlanEntryCustomRuntimeEmulator = "aws-custom-runtime-emulator-extension"
+)
+
 func (d *Detect) Detect(context packit.DetectContext) (packit.DetectResult, error) {
-	return packit.DetectResult{}, nil
+	return packit.DetectResult{
+		Plan: packit.BuildPlan{
+			Provides: []packit.BuildPlanProvision{
+				{Name: PlanEntryAwsLambda},
+			},
+			Requires: []packit.BuildPlanRequirement{
+				{Name: PlanEntryAwsLambda},
+				{Name: PlanEntryCustomRuntimeEmulator},
+			},
+		},
+	}, nil
 }

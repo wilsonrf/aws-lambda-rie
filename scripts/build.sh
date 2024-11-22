3b3d8371c041fb40bb4c5bb14b701965666294d5
Wilson da Rocha Franca <wilsonrf@gmail.com>
2024-11-22 14:51:35 +0100

    fix the the build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,8 +5,8 @@ GOMOD=$(head -1 go.mod | awk '{print $2}')
 GOOS="linux" GOARCH="amd64" go build -ldflags='-s -w' -o linux/amd64/bin/main "$GOMOD/cmd/main"
 GOOS="linux" GOARCH="amd64" go build -ldflags='-s -w' -o bin/main "$GOMOD/cmd/main"
 
-ln -fs main linux/amd64/bin/generate
+ln -fs main linux/amd64/bin/build
 ln -fs main linux/amd64/bin/detect
 
-ln -fs main bin/generate
+ln -fs main bin/build
 ln -fs main bin/detect
